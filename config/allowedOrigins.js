342ad76342abc0934abe48e94829ae8313933e83
Jovan Milojevic <jovanm028@gmail.com>
2023-04-21 21:31:28 +0200

    origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     "http://127.0.0.1:5173/",
     "http://localhost:3000/",
     "https://bauprojekt-test2.vercel.app/",
+    "https://bauprojekt-be.vercel.app/",
   ];
   
   module.exports = allowedOrigins
